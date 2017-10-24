1f64e8b6df99560e2660b6c6f831c109c2544d42
bbalconi <bbalconi@gmail.com>
2017-10-24 16:35:24 -0600

    tues 4:35

--- potluck/src/JoinHouse/joinHouse.js
+++ potluck/src/JoinHouse/joinHouse.js
@@ -9,7 +9,6 @@ class JoinHouse extends Component{
   constructor(){
     super();
     this.inputjoinHouse = this.inputjoinHouse.bind(this);
-    this.inputemailChange = this.inputemailChange.bind(this);
     this.inputpasswordChange = this.inputpasswordChange.bind(this);
     this.joint = this.joint.bind(this);
     this._handleKeyPress = this._handleKeyPress.bind(this);
@@ -23,9 +22,6 @@ class JoinHouse extends Component{
   inputjoinHouse(event) {
     this.setState({joinHouse: event.target.value});
   }
-  inputemailChange(event) {
-    this.setState({email: event.target.value});
-  }
   inputpasswordChange(event) {
     this.setState({password: event.target.value});
   }
@@ -35,20 +31,15 @@ class JoinHouse extends Component{
             password: this.state.password
           })
     .then((userObj) => {
-      console.log(userObj);
-      console.log(this.state);
       if (userObj.data.message === "House Does Not Match") {
         this.setState({
           message: userObj.data.message
         })  
       }  else {
-        
         this.setState({
           message: userObj.data.message,
           joinHouse: '',
           password: '',
-  
-
         });
         this.props.history.push("/main");
       }
@@ -74,11 +65,6 @@ _handleKeyPress(e){
         <FormGroup className='joinhouse-input'>
           <Label for="houseName">Enter House Name</Label>{' '}
           <Input type="text" onChange={this.inputjoinHouse} value={this.state.joinHouse} name="houseName" id="houseName" onKeyPress={this._handleKeyPress}  />
-        </FormGroup>
-        {' '}
-        <FormGroup>
-          <Label for="houseName">Enter Email</Label>{' '}
-          <Input type="text" onChange={this.inputUser} value={this.state.user} name="houseName" id="houseName" onKeyPress={this._handleKeyPress}  />
         </FormGroup>
         {' '}
         <FormGroup>
